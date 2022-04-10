e773d13c9c880596addc4a8431a38bdc770b3ab2
Ken Shay <kennyshay123@gmail.com>
2022-04-10 13:46:08 -0400

    Initial Code

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
-async def send_slack_channel_notification(nuula_request_info_dict):
+import json
+import httpx
+import asyncio
 
-    nuula_request_header = nuula_request_info_dict.get('header',{})
-    nuula_request_payload = nuula_request_info_dict.get('payload',{})
 
-    nuula_request_payload_str = f"\n####### INTERNAL INFO #########\n\n"
-
-    nuula_request_payload_str += f"*destination_url*: " \
-        f" {nuula_request_info_dict.get('api_web_hook_nuula','')}\n" \
-        f"*internal_event*: " \
-        f"{nuula_request_info_dict.get('internal_event','')}\n"
-
-    nuula_request_payload_str += f"\n####### REQUEST HEADER #########\n\n"
-
-    for key in nuula_request_header.keys():
-        if key == AUTH_HEADER:
-            line = f"*{key}*: ***********\n"
-        else:
-            line = f"*{key}*: {nuula_request_header.get(key, '')}\n"
-        nuula_request_payload_str += line
-
-    nuula_request_payload_str += f"\n####### REQUEST PAYLOAD #########\n\n"
-
-    for key in nuula_request_payload.keys():
-        line = f"*{key}*: {nuula_request_payload.get(key, '')}\n"
-        nuula_request_payload_str += line
-
+async def send_slack_channel_notification():
     message = {
         "blocks": [
             {
@@ -34,7 +13,7 @@ async def send_slack_channel_notification(nuula_request_info_dict):
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": nuula_request_payload_str
+                    "text": 'test'
                 }
             }
         ]
@@ -43,10 +22,17 @@ async def send_slack_channel_notification(nuula_request_info_dict):
     slack_payload = json.dumps(message).encode("utf-8")
     slack_headers = [(b"content-type", b"application/json")]
 
-    slack_url = nuula_request_info_dict.get("slack_url")
+    slack_url = "https://hooks.slack.com/services/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"
 
     if slack_url:
         async with httpx.AsyncClient() as client:
             await client.post(
-                slack_url, headers=slack_headers, data=slack_payload
-            )
+                    slack_url, headers=slack_headers, data=slack_payload
+                )
+
+
+
+
+
+asyncio.run(send_slack_channel_notification())
+
