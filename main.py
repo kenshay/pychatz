ab4b767d731eef79c52ba670116882056c7a5903
kirjeo <jshay1949@gmail.com>
2022-04-10 15:44:09 -0400

    Changed the WebHook we use

--- main.py
+++ main.py
@@ -22,16 +22,13 @@ async def send_slack_channel_notification():
     slack_payload = json.dumps(message).encode("utf-8")
     slack_headers = [(b"content-type", b"application/json")]
 
-    slack_url = "https://hooks.slack.com/services/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"
+    slack_url = "https://hooks.slack.com/services/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"
 
 
     async with httpx.AsyncClient() as client:
         await client.post(
                 slack_url, headers=slack_headers, data=slack_payload
             )
-
-
-
 
 
 asyncio.run(send_slack_channel_notification())
